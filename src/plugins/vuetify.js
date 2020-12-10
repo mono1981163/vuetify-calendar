5cb30b31abf9ae9db2f041aba9e9c4e48fdb2fc4
mo no <39118305+mono1981163@users.noreply.github.com>
2020-12-11 03:51:52 +0900

    Complete UI

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,5 +3,13 @@ import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
 
+import VueCookie from 'vue-cookie'
+import '@mdi/font/css/materialdesignicons.css'
+// Tell Vue to use the plugin
+
+Vue.use(VueCookie);
 export default new Vuetify({
+    icons: {
+        iconfont: 'mdi', 
+    }
 });
